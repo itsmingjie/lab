7d563846bd11e546baf573c350db6e94fe772aa9
Mingjie Jiang <jiang@mingjie.info>
2020-01-20 16:24:34 -0500

    Report CPU temp

--- online.sh
+++ online.sh
@@ -17,10 +17,14 @@ PREV_TIME=$(cat online.tmp)
 
 echo $TIME > online.tmp
 
+# CPU Temperature
+CPU_TEMP=$(</sys/class/thermal/thermal_zone0/temp)
+CPU_TEMP_H=$(echo "$((CPU_TEMP/1000*9/5 + 32)) °F")
+
 # Script to run after establishing remote connection
 
 IP=$(hostname --ip-address)
-ONLINE_MSG=":arrow_up: *$HOSTNAME* is checking in online at $TIME. Previous check-in occured at $PREV_TIME."
+ONLINE_MSG=":arrow_up: *$HOSTNAME* is checking in online at $TIME, current CPU temperature *$CPU_TEMP_H*. Previous check-in occured at $PREV_TIME."
 
 echo $ONLINE_MSG
 
