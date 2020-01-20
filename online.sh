e0b7f85c4e52849bea7f9094ba93c6674559ae42
Mingjie Jiang <jiang@mingjie.info>
2020-01-20 16:15:41 -0500

    Update

--- online.sh
+++ online.sh
@@ -11,12 +11,17 @@ read_var() {
 
 # Load Webhook
 HOOK=$(read_var LAB_HOOK .env)
+TIME=$(date +"%D %T")
+
+PREV_TIME=$(cat online.tmp)
+
+echo $TIME > online.tmp
 
 # Script to run after establishing remote connection
 
 IP=$(hostname --ip-address)
-ONLINEMSG=":arrow_up: *$HOSTNAME* is online at *$IP*. "
+ONLINE_MSG=":arrow_up: *$HOSTNAME* is checking in online at $TIME. Previous check-in occured at $PREV_TIME."
 
-echo $ONLINEMSG
+echo $ONLINE_MSG
 
-curl -X POST -H 'Content-type: application/json' --data '{"text": "'"$ONLINEMSG"'"}' $HOOK
+curl -X POST -H 'Content-type: application/json' --data '{"text": "'"$ONLINE_MSG"'"}' $HOOK
