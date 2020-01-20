c7d698d5640eb6df30ccc68076344ea31f79ddae
Mingjie Jiang <jiang@mingjie.info>
2020-01-20 15:57:13 -0500

    Update script

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 # Install service
 
-sudo echo "# post status message after networking is up" >> cat /etc/network/interfaces
-sudo echo "post-up /home/mingjie/lab/online.sh" >> cat /etc/network/interfaces
+sudo echo "# post status message after networking is up" >> /etc/network/interfaces
+sudo echo "post-up /home/mingjie/lab/online.sh" >> /etc/network/interfaces
