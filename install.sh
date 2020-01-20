1d0af166d55775a34718b5e07ffc276e32d62aed
Mingjie Jiang <jiang@mingjie.info>
2020-01-20 15:56:37 -0500

    Elevate

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 # Install service
 
-echo "# post status message after networking is up" >> cat /etc/network/interfaces
-echo "post-up /home/mingjie/lab/online.sh" >> cat /etc/network/interfaces
+sudo echo "# post status message after networking is up" >> cat /etc/network/interfaces
+sudo echo "post-up /home/mingjie/lab/online.sh" >> cat /etc/network/interfaces
