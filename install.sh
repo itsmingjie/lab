69a9a14e367db310866bd303cd57b6cd1c5ab17e
Mingjie Jiang <jiang@mingjie.info>
2020-01-20 15:48:36 -0500

    Update command

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 # Install service
 
-sudo ln -s pi-online.service /etc/systemd/system/pi-online.service
+sudo systemctl link /home/mingjie/lab/pi-online.service
 sudo systemctl enable pi-online.service
 sudo systemctl start pi-online.service
