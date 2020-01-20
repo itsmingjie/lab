c8d87749463437ff03d68ec27b6e9780643f1d57
Mingjie Jiang <jiang@mingjie.info>
2020-01-20 15:34:48 -0500

    Initial script configuration

---
+++ online.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Read environment variables from .env files
+read_var() {
+    VAR=$(grep $1 $2 | xargs)
+    IFS="=" read -ra VAR <<< "$VAR"
+    echo ${VAR[1]}
+}
+
+# Load Webhook
+HOOK=$(read_var LAB_HOOK .env)
+
+# Script to run after establishing remote connection
+
+IP=$(hostname --ip-address)
+ONLINEMSG=":arrow_up: *$HOSTNAME* is online at *$IP*. "
+
+echo $ONLINEMSG
+
+curl -X POST -H 'Content-type: application/json' --data '{"text": "'"$ONLINEMSG"'"}' $HOOK
